159888d23132ee183ba37f979ee2d7365a1a6bc0
Gummi Ng <thaonp411@gmail.com>
2025-01-03 09:55:27 +0700

    feat: update package

--- blog/index.js
+++ blog/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const morgan = require("morgan");
+
+app.use(morgan("combined"));
 //route
 app.get("/tin-tuc", (req, res) => {
   res.send("Hello World!");
